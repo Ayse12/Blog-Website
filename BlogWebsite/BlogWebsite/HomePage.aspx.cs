0e22c9484a6447236fcaefc7b3a5184d569d60ff
Ayşe İlhanlı <ayseilhanl67@gmail.com>
2020-09-10 11:23:47 +0300

    HomePage.aspx
    The page HomePage.aspx was designed and the data in the database is with datalist were integrated into the design.

--- BlogWebsite/BlogWebsite/HomePage.aspx.cs
+++ BlogWebsite/BlogWebsite/HomePage.aspx.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace BlogWebsite
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        _1_SqlCon sql = new _1_SqlCon();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            SqlCommand cmd = new SqlCommand("Select * From Articles", sql.Connec());
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataList2.DataSource = dr;
+            DataList2.DataBind();
         }
     }
 }
